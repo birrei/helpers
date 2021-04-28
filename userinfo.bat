5e0166ab91783c8ab71a593b5cb94bdb31f5c225
birgitreiner <birgitreiner@outlook.de>
2021-04-28 17:51:59 +0200

    neu

---
+++ userinfo.bat
@@ -0,0 +1,12 @@
+@echo off 
+chcp 1252
+
+REM script org: OneDrive: ..\Projekte\Scriptsammlungen\Batch2\config.bat
+
+REM ~ ECHO  USERPROFILE: %USERPROFILE%
+REM ~ ECHO  COMPUTERNAME: %COMPUTERNAME%
+
+SET DIR_DOWNLOADS=%USERPROFILE%\Downloads
+
+REM ~ ECHO DIR_DOWNLOADS: %DIR_DOWNLOADS%
+
